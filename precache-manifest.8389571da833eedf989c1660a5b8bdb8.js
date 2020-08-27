ba74d4915d3e71a20b52c8649f923b003b2a332a
Dang Trong Tai <taidt.bhsoft@gmail.com>
2020-08-27 09:01:07 +0700

    Updates

---
+++ precache-manifest.8389571da833eedf989c1660a5b8bdb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a8499a90568cd25c5a30f3782bfb0a",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "85425a4f3bd006352e6e",
+    "url": "/todolist/static/css/main.bab9e64d.chunk.css"
+  },
+  {
+    "revision": "8e011e00fbe89ee31167",
+    "url": "/todolist/static/js/2.2e161456.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todolist/static/js/2.2e161456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85425a4f3bd006352e6e",
+    "url": "/todolist/static/js/main.d0fca614.chunk.js"
+  },
+  {
+    "revision": "0447b1840d849e6cf4b0",
+    "url": "/todolist/static/js/runtime-main.98597097.js"
+  }
+]);
